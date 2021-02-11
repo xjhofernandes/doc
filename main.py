dc1757048beb2fe011154586bf382ebc9add107d
Jonathan Fernandes <jhofernandes@pop-os.localdomain>
2021-02-11 18:16:02 -0300

    Adding modifications

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-def hello():
-    """Please, say 'WORLD'"""
-    return "WORLD"
+def find_noob():
+    """ Usually the return is 'Eric' 
+
+    :returns: Eric, obvious
+    """
+    return "Eric"
+
+def search_name(name):
+    """ Search a name in MongoDb Collection.
+
+    :param name: String and the first of my arguments (use any name ;).
+
+    :returns: Find or not find name (100% complicated)
+
+    """
+
+    list_names = ['Joseph', 'Mariah', 'Carl']
+    if name in list_names:
+        return "Found!"
+    else:
+        return "Sorry, i can't find."
+
+
+print(find_noob())
+search_name('Teste')
