274935bea4c15103ea4058d691e544902b6ac0fe
Jonathan Fernandes <jhofernandes@pop-os.localdomain>
2021-02-13 04:23:20 -0300

    updating

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,9 @@ release = '2021'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosectionlabel']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -55,6 +57,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_rtd_theme'
 #html_theme = 'alabaster'
+autosectionlabel_prefix_document = True
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- main.py
+++ main.py
@@ -27,5 +27,33 @@ def new_function():
     """
     return "Teste :)"        
 
-print(find_noob())
-search_name('Teste')
+def binary_search(arr, x):
+    """ In a nutshell, this search algorithm takes advantage of a collection of elements that is already sorted by ignoring half of the elements after just one comparison. 
+
+    :param arr: Integer Array
+    :param x: Integer number
+
+    :returns: If the number really exist in array the function will return index of the number in integer array. Else, the function will return -1.
+   
+    """    
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+
+    while low <= high: 
+        mid = (high + low) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+
+    return -1
+
+arr = [ 2, 3, 10, 4, 40 ]
+x = 10
+
+result = binary_search(arr, x)
+
+print(result)
